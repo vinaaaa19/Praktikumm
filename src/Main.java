d9ef0a1c99a72fc6884fba1ed163b4c1e7ae7e8e
vinaaaa19 <pinaafiru@gmail.com>
2025-10-22 17:46:31 +0700

    menambahkan

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,21 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Masukkan jumlah angka: ");
+        System.out.print("Masukkan jumlah angka: ");
         int n = scanner.nextInt();
 
         int[] angka = new int[n];
-        System.out.println("Masukkan angka-angka: ");
+        System.out.print("Masukkan angka-angka: ");
         for (int i = 0; i<n; i++){
             angka[i] = scanner.nextInt();
         }
-
         int total = 0;
         for (int i = 0; i < n; i++){ //pada loop dibagian ini seharusnya dimulai dari 0
             total += angka[i];
         }
 
         double rataRata = (double) total/n;
-        System.out.println("Rata-rata adalah: " + rataRata);
+        System.out.print("Rata-rata adalah: " + rataRata);
 
         scanner.close();
     }
